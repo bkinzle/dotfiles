04aa5f8c2e902ff5a5f4fcd0576b2f31da3c2b14
Brandt Kinzle <114622981+bkinzle@users.noreply.github.com>
2026-04-02 13:51:27 -0500

    feat: remove use of fzf for kubectx and kubens

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,5 @@
 alias brewu="brew update && brew upgrade && brew cleanup"
-alias kx=kubectx kn=kubens gc=gcloud k=kubectl kaf=kafkactl
+alias kx='KUBECTX_IGNORE_FZF=1 kubectx' kn='KUBECTX_IGNORE_FZF=1 kubens' gc=gcloud k=kubectl kaf=kafkactl
 alias duckdb-init='duckdb -init "$HOME/Documents/keep/duckdb-to-gcs.txt" -json'
 alias dprune='docker system prune --all --volumes --force && docker volume prune --all --force'
 alias cat='bat --paging=never --plain'

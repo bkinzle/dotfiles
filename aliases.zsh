e8a2e3910fd8f3e046e477f3f1b0cf6a72e4b561
Brandt Kinzle <114622981+bkinzle@users.noreply.github.com>
2026-05-09 14:31:24 -0600

    feat: add kubectl-tree and kube-lineage

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,11 @@
 alias brewu="brew update && brew upgrade && brew cleanup"
-alias kx='KUBECTX_IGNORE_FZF=1 kubectx' kn='KUBECTX_IGNORE_FZF=1 kubens' gc=gcloud k=kubectl kaf=kafkactl
+alias kx='KUBECTX_IGNORE_FZF=1 kubectx'
+alias kn='KUBECTX_IGNORE_FZF=1 kubens'
+alias k=kubectl
+alias kaf=kafkactl
+alias kt=kubectl-tree
+alias kl=kube-lineage
+alias gc=gcloud
 alias duckdb-init='duckdb -init "$HOME/Documents/keep/duckdb-to-gcs.txt" -json'
 alias dprune='docker system prune --all --volumes --force && docker volume prune --all --force'
 alias cat='bat --paging=never --plain'
